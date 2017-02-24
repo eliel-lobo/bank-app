f214573ac96c4a14db744bf6aee32fa0c3463151
Eliel Lobo <elobo@cvent.com>
2017-02-24 18:20:55 -0500

    Manage user session (login / logout)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get    'sessions/new'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  root :to => "users#new"
+
   resources :account_movements
   resources :accounts
   resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
